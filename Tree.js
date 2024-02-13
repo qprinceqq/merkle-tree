8a8d36f45e945eb0222573ddb5177c4e8f4a31a8
labintsev <andrej.labintsev@yandex.ru>
2024-02-13 12:33:23 +0300

    solve Tree

--- Tree.js
+++ Tree.js
@@ -16,12 +16,59 @@ class Tree {
         this.root = null;
     }
 
+    addNext(parent, child){
+        if (child.data <= parent.data){
+            if (parent.left == null){
+                parent.left = child;
+                return;
+            } else {
+                this.addNext(parent.left, child);
+                return;
+            }
+        } else {
+            if (parent.right == null){
+                parent.right = child;
+                return;
+            } else {
+                this.addNext(parent.right, child);
+                return;
+            }
+        }
+    }
+
     addNode(node){
-        // TODO 1 Implement 
+        // Implement 
+        if (this.root == null){
+            this.root = node;
+            return;
+        }
+        this.addNext(this.root, node);
+    }
+
+    hasNext(parent, data){
+        if (parent.data == data){
+            return true;
+        } else {
+            if (data < parent.data){
+                if (parent.left == null){
+                    return false;
+                } else {
+                    return this.hasNext(parent.left, data);
+                }
+            }         
+            if (data > parent.data){
+                if (parent.right == null){
+                    return false;
+                } else {
+                    return this.hasNext(parent.right, data);
+                }
+            }
+        }
     }
 
     hasNode(data){
-        // TODO 2 Implement 
+        // Implement 
+        return this.hasNext(this.root, data);
     }
 }
 
